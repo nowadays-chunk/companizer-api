c0093152c8a25c30d07fd50a0d23f7d89effc2f4
Hamza Eljaouhari <h.eljaouhari.home@gmail.com>
2026-02-06 10:33:05 +0100

    PRODUCTION DB DEPLOY TO RENDER.COM CLOUD

--- src/db.js
+++ src/db.js
@@ -5,18 +5,18 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 console.log("DB Config: Host:", process.env.DB_HOST, "User:", process.env.DB_USER); // Debug log
 
 
-const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_HOST = process.env.DB_HOST || "sql7.freesqldatabase.com";
 const DB_PORT = process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306;
 const DB_USER =
     process.env.DB_USER && process.env.DB_USER.trim()
         ? process.env.DB_USER.trim()
-        : "root";
+        : "sql7815947";
 const DB_PASSWORD =
     process.env.DB_PASSWORD && process.env.DB_PASSWORD.length
         ? process.env.DB_PASSWORD
-        : "P@ssw0rd"; // empty password (local dev)
+        : "mbtpwhmXvB"; // empty password (local dev)
 
-const DB_NAME = process.env.DB_NAME || "companizer_db_prod";
+const DB_NAME = process.env.DB_NAME || "sql7815947";
 
 const pool = mysql.createPool({
     host: DB_HOST,
